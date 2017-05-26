4798b75a280ecaedf8f014487d4b2cf5951ff86c
Shanmugavel Arunachalam <iashanmugavel@gmail.com>
2017-05-26 22:38:50 +0530

    First Commit

---
+++ calc.rb
@@ -0,0 +1,17 @@
+#addition of two numbers stackoverflow
+puts "entrer the first number"
+a = gets.chomp.to_i
+puts "enter second number"
+b = gets.chomp.to_i
+puts "addtion of a and b = #{ a + b }"
+puts "subraction of two numbers a and b = #{ a - b }"
+
+puts "multiplication of two numbers a and b = #{ a * b }"
+
+puts "division of two numbers a and b = #{ a % b }"
+
+
+x = "(2,44,555,6666,0,5)"
+y = "(3,6,888,5,8)"
+puts x.reverse
+
